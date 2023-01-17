c1c989112fd21ec772be94de42fa11782689342d
harpoonjs <w3st.io2021@gmail.com>
2023-01-16 22:15:40 -0500

    Major edits

--- contracts/FSPAuditKillers.sol
+++ contracts/FSPAuditKillers.sol
@@ -1,86 +1,93 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+
 /* [import][foreign] */
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-import "./ERC721Enumerable.sol";
 
-
+/**
+ * @title FSPAuditKillers
+ * @notice NFTs for the Friend Stock Picks Audit Killers
+*/
 contract FSPAuditKillers is
     ERC721Enumerable,
     Ownable
 {
+    /* [event] */
     event PauseEvent(bool pause);
-    event welcomeToMekaVerse(uint256 indexed id);
 
 
+    /* [using] */
     using SafeMath for uint256;
     using Counters for Counters.Counter;
 
 
-    uint256 public constant MAX_ELEMENTS = 10000;
+    /* [uint256][public][constant] */
+    uint256 public constant TOTAL_SUPPLY = 10000;
     uint256 public constant PRICE = 0.01 ether;
     uint256 public constant START_AT = 1;
 
+
+    /* [bool][private] */
     bool private PAUSE = true;
 
-    Counters.Counter private _tokenIdTracker;
 
+    /* [string][public] */
     string public baseTokenURI;
 
 
+    Counters.Counter private _tokenIdTracker;
+
+
+    /* [constructor] */
     constructor (string memory baseURI)
-        ERC721("MekaVerse", "MEKA")
+        ERC721("FSP Audit Killers", "FSP")
     {
         setBaseURI(baseURI);
     }
 
 
-    modifier saleIsOpen {
-        require(totalToken() <= MAX_ELEMENTS, "Soldout!");
+    /* [modifier] */
+    modifier saleIsOpen()
+    {
+        require(totalToken() <= TOTAL_SUPPLY, "Total reached");
         require(!PAUSE, "Sales not open");
+
         _;
     }
 
 
-    function _baseURI() internal view virtual override returns (string memory) {
+    /* [function] */
+    function _baseURI()
+        internal
+        view
+        virtual
+        override
+        returns (string memory)
+    {
         return baseTokenURI;
     }
 
 
-    function setBaseURI(string memory baseURI) public onlyOwner {
+    function setBaseURI(string memory baseURI)
+        public
+        onlyOwner()
+    {
         baseTokenURI = baseURI;
     }
 
 
-    function totalToken() public view returns (uint256) {
+    function totalToken()
+        public
+        view
+        returns (uint256)
+    {
         return _tokenIdTracker.current();
-    }
-
-
-    function mint(uint256[] memory _tokensId, uint256 _timestamp, bytes memory _signature) public payable saleIsOpen {
-
-        uint256 total = totalToken();
-        require(_tokensId.length <= 2, "Max limit");
-        require(total + _tokensId.length <= MAX_ELEMENTS, "Max limit");
-        require(msg.value >= price(_tokensId.length), "Value below price");
-
-        address wallet = _msgSender();
-
-        address signerOwner = signatureWallet(wallet,_tokensId,_timestamp,_signature);
-        require(signerOwner == owner(), "Not authorized to mint");
-
-        require(block.timestamp >= _timestamp - 30, "Out of time");
-
-        for(uint8 i = 0; i < _tokensId.length; i++){
-            require(rawOwnerOf(_tokensId[i]) == address(0) && _tokensId[i] > 0 && _tokensId[i] <= MAX_ELEMENTS, "Token already minted");
-            _mintAnElement(wallet, _tokensId[i]);
-        }
-
     }
 
 
@@ -101,13 +108,49 @@ contract FSPAuditKillers is
     }
 
 
+    function mint(
+        uint256[] memory _tokensId,
+        uint256 _timestamp,
+        bytes memory _signature
+    )
+        public
+        payable
+        saleIsOpen()
+    {
+        require(totalToken() + _tokensId.length <= TOTAL_SUPPLY, "Total supply reached");
+
+        require(msg.value >= price(_tokensId.length), "Value below price");
+
+        address signerOwner = signatureWallet(
+            _msgSender(),
+            _tokensId,
+            _timestamp,
+            _signature
+        );
+
+        require(signerOwner == owner(), "Not authorized to mint");
+
+        require(block.timestamp >= _timestamp - 30, "Out of time");
+
+        for (uint8 i = 0; i < _tokensId.length; i++)
+        {
+            require(
+                ownerOf(_tokensId[i]) == address(0) &&
+                _tokensId[i] > 0 &&
+                _tokensId[i] <= TOTAL_SUPPLY,
+                "Token already minted"
+            );
+
+            _mintAnElement(_msgSender(), _tokensId[i]);
+        }
+    }
+
+
     function _mintAnElement(address _to, uint256 _tokenId)
         private
     {
         _tokenIdTracker.increment();
         _safeMint(_to, _tokenId);
-
-        emit welcomeToMekaVerse(_tokenId);
     }
 
 
@@ -128,7 +171,8 @@ contract FSPAuditKillers is
         uint256 tokenCount = balanceOf(_owner);
 
         uint256[] memory tokensId = new uint256[](tokenCount);
-        for (uint256 i = 0; i < tokenCount; i++) {
+        for (uint256 i = 0; i < tokenCount; i++)
+        {
             tokensId[i] = tokenOfOwnerByIndex(_owner, i);
         }
 
@@ -138,7 +182,7 @@ contract FSPAuditKillers is
 
     function setPause(bool _pause)
         public
-        onlyOwner
+        onlyOwner()
     {
         PAUSE = _pause;
         emit PauseEvent(PAUSE);
@@ -158,12 +202,13 @@ contract FSPAuditKillers is
         view
         returns (uint256[] memory)
     {
-
         uint256[] memory tokens = new uint256[](limit);
 
-        for (uint256 i = 0; i < limit; i++) {
+        for (uint256 i = 0; i < limit; i++)
+        {
             uint256 key = i + offset;
-            if(rawOwnerOf(key) == address(0)){
+            if (ownerOf(key) == address(0))
+            {
                 tokens[i] = key;
             }
         }
@@ -174,13 +219,14 @@ contract FSPAuditKillers is
 
     function mintUnsoldTokens(uint256[] memory _tokensId)
         public 
-        onlyOwner
+        onlyOwner()
     {
-
         require(PAUSE, "Pause is disable");
 
-        for (uint256 i = 0; i < _tokensId.length; i++) {
-            if(rawOwnerOf(_tokensId[i]) == address(0)){
+        for (uint256 i = 0; i < _tokensId.length; i++)
+        {
+            if (ownerOf(_tokensId[i]) == address(0))
+            {
                 _mintAnElement(owner(), _tokensId[i]);
             }
         }

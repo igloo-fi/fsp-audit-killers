6478dfaa75c4fcd585b340266697fb3d082959e1
harpoonjs <w3st.io2021@gmail.com>
2023-01-22 16:47:09 -0500

    successful deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,30 @@
+require("dotenv").config()
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+
+/**
+ * @type import("hardhat/config").HardhatUserConfig
+ */
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,19 +1,18 @@
 import { ethers } from "hardhat";
 
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+	console.log(`Deploying contracts with the account: ${deployer.address}`);
+	console.log(`Account Balance: ${await deployer.getBalance()}`);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const FSPAuditKillers = await ethers.getContractFactory("FSPAuditKillers");
+  const _FfSPAuditKillers = await FSPAuditKillers.deploy("https://");
 
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`_FfSPAuditKillers deployed at address: ${_FfSPAuditKillers.address}`);
 }
+
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
